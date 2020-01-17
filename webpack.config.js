9ac7a70a4723d3278f4e36c57f652349d58122e9
Dilakv <ingfvalencia@gmail.com>
2020-01-17 00:45:10 -0600

    fix babel and webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -38,44 +38,4 @@ module.exports = {
 
         ],
     }
-var path = require('path');
-
-module.exports = {
-    // Change to your "entry-point".
-    entry: './src/index',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-        rules: [
-            {
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            },
-            
-            {
-                test: /\.(scss|sass|css)$/,
-                exclude: /node_modules/,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                  ],
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
-            }
-
-        ],
-    }
 };
